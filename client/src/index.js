381f05f18e529b3ce6e7b4493d397a1e98013fbd
Jino W <jinoesther@gmail.com>
2020-03-28 12:23:09 +0530

    public folder enabled

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux'
 import configureStore from './store/configureStore'
-import {startGetCategories} from '../../actions/categories'
-import {startGetNotes} from "../../actions/notes"
+import {startGetCategories} from './actions/categories'
+import {startGetNotes} from "./actions/notes"
 
 
 
